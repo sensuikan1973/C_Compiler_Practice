c3854a1bec2c84bacf58af5333065ffb5f159c9a
ShimizuNaoki <sensuikan1973@gmail.com>
2020-04-19 14:09:58 +0900

    9cc.h を作ったぞおおおおお

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,26 +1,4 @@
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// トークンの種類
-typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM,      // 整数トークン
-  TK_EOF,      // 入力の終わりを表すトークン
-} TokenKind;
-
-// トークン型
-typedef struct Token Token;
-struct Token {
-  TokenKind kind; // トークンの型
-  Token *next;    // 次の入力トークン
-  int val;        // kindがTK_NUMの場合、その数値
-  char *str;      // トークン文字列
-  int len;        // トークンの長さ。トークンでなければ 0 を入れる。
-};
+#include "9cc.h"
 
 // 入力プログラム
 char *user_input;
@@ -141,28 +119,6 @@ Token *tokenize() {
   new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
-
-// 抽象構文木のノードの種類
-typedef enum {
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_NUM, // 整数
-  ND_EQ, // ==
-  ND_NOT_EQ, // !=
-  ND_LESS , // <
-  ND_LESS_OR_EQ, // <=
-} NodeKind;
-
-// 抽象構文木のノードの型
-typedef struct Node Node;
-struct Node {
-  NodeKind kind; // ノードの型
-  Node *lhs;     // 左辺
-  Node *rhs;     // 右辺
-  int val;       // kindがND_NUMの場合のみ使う
-};
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
   Node *node = calloc(1, sizeof(Node));
