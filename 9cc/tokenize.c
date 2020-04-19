51714991fa413ddd8288262d948e35d77b6bd206
ShimizuNaoki <sensuikan1973@gmail.com>
2020-04-19 14:44:07 +0900

    add tokenize.c

---
+++ 9cc/tokenize.c
@@ -0,0 +1,57 @@
+#include "9cc.h"
+
+bool is_prefix(char *str, char *prefix) {
+  return strncmp(prefix, str, strlen(prefix)) == 0;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize() {
+  char *p = user_input;
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if(is_prefix(p, "==") || is_prefix(p, "!=") || is_prefix(p, ">=") || is_prefix(p, "<=")) {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-' ||
+        *p == '*' || *p == '/' ||
+        *p == '(' || *p == ')' ||
+        *p == '<' || *p == '>') {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      cur->val = strtol(p, &p, 10);
+      // digit の場合、長さなんぞどうでもいので、cur->len は埋めてない
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
